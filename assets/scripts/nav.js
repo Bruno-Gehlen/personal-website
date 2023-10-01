8dd5338053a5c835cfe0f53ddfb22c65c74d967c
Bruno Gehlen <85147059+Bruno-Gehlen@users.noreply.github.com>
2023-10-01 10:39:22 -0300

    Add files via upload

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,4 +1,5 @@
 const allLinks = document.querySelectorAll(".sidebar-links a")
+const allLinksH = document.querySelectorAll(".right-div a")
 
 allLinks.forEach((elem) => {
   elem.addEventListener('click', function() {
@@ -14,4 +15,16 @@ allLinks.forEach((elem) => {
   })
 });
 
+allLinksH.forEach((elem) => {
+  elem.addEventListener('click', function() {
+    const hrefLinkClick = elem.href;
 
+    allLinksH.forEach((link) => {
+      if (link.href == hrefLinkClick){
+        link.classList.add("active");
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  })
+});
