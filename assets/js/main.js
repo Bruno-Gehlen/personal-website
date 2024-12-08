941787eb810ac81306b16ccd56f2f8c6ee196248
Bruno-Gehlen <brunogfdsilva@gmail.com>
2024-12-07 23:19:31 -0300

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,22 +134,22 @@
   /**
    * Skills animation
    */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
+  // let skilsContent = select('.skills-content');
+  // if (skilsContent) {
+  //   new Waypoint({
+  //     element: skilsContent,
+  //     offset: '80%',
+  //     handler: function(direction) {
+  //       let progress = select('.progress .progress-bar', true);
+  //       progress.forEach((el) => {
+  //         el.style.width = el.getAttribute('aria-valuenow') + '%'
+  //       });
+  //     }
+  //   })
+  // }
 
   /**
-   * Testimonials slider
+   * slider
    */
   new Swiper('.testimonials-slider', {
     speed: 600,
